9ba7f8c4a0c099ea05986fb9e032871defd7af22
emeibech <emeibech@gmail.com>
2023-02-23 06:59:27 +0800

    Make the test simpler by isolating it

--- src/modules/tests/Ship.test.js
+++ src/modules/tests/Ship.test.js
@@ -2,12 +2,13 @@
 import Ship from '../Ship';
 
 test('isSunk returns true if hit count matches length', () => {
+  // default hit count is 0
   const twoLengthShip = Ship(2);
   expect(twoLengthShip.isSunk()).toBe(false);
 
-  twoLengthShip.hit();
-  expect(twoLengthShip.isSunk()).toBe(false);
-
-  twoLengthShip.hit();
-  expect(twoLengthShip.isSunk()).toBe(true);
+  const zeroLengthShip = Ship(0);
+  expect(zeroLengthShip.isSunk()).toBe(true);
 });
+
+/* .hit() method can be called to increase hit count
+  but I think that's for integration test so I removed it */
